4f69874fa4ccb083f6781289701fd05e2a8caf45
Gregory Adam Scott <sudasana@gmail.com>
2019-09-02 09:27:19 +0100

    Adding dos batch file to repo

---
+++ build.bat
@@ -0,0 +1,6 @@
+pyinstaller -F armcom2.py
+copy /y .\campaigns\*.json .\dist\campaigns
+copy /y .\data\*.json .\dist\data
+copy /y .\data\*.xp .\dist\data
+copy /y .\data\*.png .\dist\data
+copy /y .\sounds\*.ogg .\dist\sounds

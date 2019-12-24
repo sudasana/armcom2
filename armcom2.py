24367e99879fc78fb5b4296b250e20d68b678223
Sudasana <sudasana@gmail.com>
2019-12-24 16:26:37 +0000

    Update armcom2.py

--- armcom2.py
+++ armcom2.py
@@ -67,7 +67,7 @@ from calendar import monthrange			# for date calculations
 #                                        Constants                                       #
 ##########################################################################################
 
-DEBUG = True						# debug flag - set to False in all distribution versions
+DEBUG = False						# debug flag - set to False in all distribution versions
 NAME = 'Armoured Commander II'				# game name
 VERSION = '0.10.0 24-12-19'				# game version
 DATAPATH = 'data/'.replace('/', os.sep)			# path to data files
